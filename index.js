7ca5cb29a10ee71396410f6bd3c6f3079aa2c7e4
monogamus <monogamus@yahoo.com>
2019-11-05 16:57:55 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const digitA = 100
+const digitB = 200.5
+
+const resultAddition = digitA + digitB
+const resultSubstraction = digitA - digitB
+const resultMultiplication = digitA * digitB
+const resultDivison = digitA / digitB
+const resultModulus = digitB % digitA
+
+console.log(digitA)
+console.log(digitB)
+
+console.log(resultAddition)
+console.log(resultSubstraction)
+console.log(resultMultiplication)
+console.log(resultDivison)
+console.log(resultModulus)
